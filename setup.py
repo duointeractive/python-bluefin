b43f5b6aaf02f95fee2a7827ebfbabcc52cf23da
Greg Taylor <gtaylor@duointeractive.com>
2011-06-22 15:20:42 -0400

    Packaging bluefin for PyPi.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import bluefin
+
+DESCRIPTION = "A simple Bluefin Payment System API."
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+version_str = '%d.%d' % (bluefin.VERSION[0], bluefin.VERSION[1])
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='bluefin',
+    version=version_str,
+    packages=[
+        'bluefin',
+        'bluefin.directmode',
+    ],
+    author='Gregory Taylor',
+    author_email='gtaylor@duointeractive.com',
+    url='https://github.com/duointeractive/python-bluefin/',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+)
