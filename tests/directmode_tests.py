4945d7d38e0c3ef01d1d0a952021297fe03fab48
Greg Taylor <gtaylor@duointeractive.com>
2011-09-28 14:42:38 -0400

    Adding some more required parameters to bluefin tests.

--- tests/directmode_tests.py
+++ tests/directmode_tests.py
@@ -3,6 +3,38 @@ from bluefin.directmode.clients import V3Client
 from bluefin.directmode.exceptions import V3ClientInputException
 from tests.api_details import API_DETAILS, TEST_CARD
 
+class SaleTests(unittest.TestCase):
+    """
+    Tests for the 'sale' transaction type.
+    """
+    def setUp(self):
+        self.api = V3Client()
+
+    def test_basic(self):
+        """
+        Test a basic successful API call.
+        """
+        response = self.api.send_request({
+            'pay_type': 'C',
+            'tran_type': 'S',
+            'account_id': API_DETAILS['account_id'],
+            'amount': 1.0,
+            'card_number': TEST_CARD['card_number'],
+            'card_expire': TEST_CARD['card_expire'],
+            'dynip_sec_code': API_DETAILS['dynip_sec_code'],
+            # Must be the customer's IP address.
+            'cust_ip': '127.0.0.1',
+            'bill_name1': 'First',
+            'bill_name2': 'Last',
+            'bill_street': '123 Some Street',
+            'bill_zip': '12345',
+            # Two-letter country code.
+            # http://www.iso.org/iso/en/prods-services/iso3166ma/index.html
+            'bill_country': 'US',
+        })
+        self.assertEqual(response['status_code'], '1')
+
+
 class AuthorizeTests(unittest.TestCase):
     """
     Tests for authorization API calls.
@@ -22,6 +54,15 @@ class AuthorizeTests(unittest.TestCase):
             'card_number': TEST_CARD['card_number'],
             'card_expire': TEST_CARD['card_expire'],
             'dynip_sec_code': API_DETAILS['dynip_sec_code'],
+            # Must be the customer's IP address.
+            'cust_ip': '127.0.0.1',
+            'bill_name1': 'First',
+            'bill_name2': 'Last',
+            'bill_street': '123 Some Street',
+            'bill_zip': '12345',
+            # Two-letter country code.
+            # http://www.iso.org/iso/en/prods-services/iso3166ma/index.html
+            'bill_country': 'US',
         })
 
     def test_invalid_sec_code(self):
